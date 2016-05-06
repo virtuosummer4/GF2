bb3dc996fcd4a26cccc824eaa880369324abd9d3
virtuosummer4 <kr394@cam.ac.uk>
2016-05-06 13:09:04 +0100

    Done ex6-8
    Finished and debugged 6, wrote code for 7 and 8. Not sure how to create
    a makefile for windows.

--- small.cc
+++ small.cc
@@ -3,21 +3,103 @@
 // define for numbers in ex3
 //#define dnum
 // define for ex4
-#define strex4
+//#define strex4
 
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
 #include <cctype>
 #include <string>
+#include <vector>
 using namespace std;
 
 const int maxlength = 8; // maximum number of characters in a name string
 typedef string namestring;
+typedef int name;
+typedef unsigned int length;
+
+struct nameitem {
+	name index;
+	namestring str;
+};
+
+class namebase
+{
+	vector<nameitem> namelist; 
+	
+	public:
+	name add(namestring str1)
+	{
+		int index = lookup(str1);
+		if (index<0)
+		{
+			nameitem tempname;
+			index = namelist.size();
+			tempname.index = index;
+			tempname.str = str1;
+			namelist.push_back(tempname);
+		}
+		return index;
+	}
+	name lookup(namestring str)
+	{
+		name index = -1;
+		int size = namelist.size();
+		for (int i=0; i<size; i++)
+		{
+			if (str.compare(namelist[i].str)==0)
+			{
+				index = namelist[i].index;
+				break;
+			}
+		}	
+		//cout << index << endl; 
+		return index;
+	}
+	
+	void writename(name id)
+	{
+		int size = namelist.size();
+		if (id<0)
+			cout<< "Invalid name ID, use positive integers smaller than "<< size << endl;
+		else if (id>size-1)
+			cout << "Invalid name ID, use positive integers smaller than "<< size << endl;
+		else cout << namelist[id].str ;
+	}
+	
+	length namelength(name id)
+	{
+		int size = namelist.size();
+		if (id<0)
+			cout<< "Invalid name ID, use positive integers smaller than "<< size << endl;
+		else if (id>size-1)
+			cout << "Invalid name ID, use positive integers smaller than "<< size << endl;
+		else
+		{
+			length stringlength = namelist[id].str.size();
+			cout << stringlength << endl;
+			return stringlength;
+		}
+	}
+	
+	void printall()
+	{
+		int size = namelist.size();
+		for (int i=0; i<size; i++)
+		{
+			cout<< namelist[i].index<< '.'<<'\t'<<namelist[i].str<<endl;
+		}
+	}
+	
+	
+};
+
+namebase Namebase;
 
 void skipspaces(ifstream *infp, char &curch, bool &eofile);
 void getnumber(ifstream *infp, char &curch, bool &eofile, int &number);
-void getname(ifstream *infp, char &curch, bool &eofile, namestring &str);
+name getname(ifstream *infp, char &curch, bool &eofile, namestring &str);
+name lookup(namestring str);
 
 int main(int argc, char **argv)
 // If this program is put in a file called small.cc, it can
@@ -28,7 +110,7 @@ int main(int argc, char **argv)
   bool eofile;  // true when end of file is reached
   int number;	// integer to store number acquired from getnumber function.
   namestring str = "";
-  
+  name nameindex;
   
   if (argc != 2) {
     cout << "Usage:         " << argv[0] << " [filename]" << endl;
@@ -52,18 +134,18 @@ int main(int argc, char **argv)
 	  else if (isdigit(ch))
 		 {
 			 getnumber(&inf, ch, eofile, number);
-			 cout<< number<< endl;
+			 //cout<< number<< endl;
 			 if (eofile) break;
 		 }
 	  else if (isalpha(ch))
 		 {
-			 getname(&inf, ch, eofile, str);
-			 cout<< str<< endl;
+			 nameindex = getname(&inf, ch, eofile, str);
+			 cout<< nameindex << '\t' << str<< endl;
 			 if (eofile) break;
 		 }
   }
   
-
+	Namebase.printall();
   inf.close();
 }
 
@@ -99,13 +181,14 @@ void getnumber(ifstream *infp, char &curch, bool &eofile, int &number)
 	return;
 }
 
-void getname(ifstream *infp, char &curch, bool &eofile, namestring &str)
+name getname(ifstream *infp, char &curch, bool &eofile, namestring &str)
 // Reads name from text file, starting with curch
 // str = reference parameter for result
 // 
 {
 	str = "";
 	int i = 0;
+	name index;
 	
 	while (!eofile) {
 		if (!isalpha(curch)) break; // 
@@ -118,5 +201,9 @@ void getname(ifstream *infp, char &curch, bool &eofile, namestring &str)
 		cout<< "Warning: name '" << str << "' was truncated" <<endl;
 		str.resize(maxlength);
 	}
-	return;
+	index = Namebase.add(str);
+	//cout << index << endl;
+	return index;
 }
+
+
